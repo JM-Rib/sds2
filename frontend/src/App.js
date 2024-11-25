4f7335d38bb3699684d269a76858e69fd5f95b4a
lrluque <lucasrluque@icloud.com>
2024-11-26 00:36:14 +0100

    frontend lil fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,16 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Home from './views/home.js';
 import './App.css';
-import { io } from 'socket.io-client';
-import { useEffect, useState, useRef } from "react";
 
 function App() {
-    const [message, setMessage] = useState('');
-    const [messageReceived, setMessageReceived] = useState('');
-    const socketRef = useRef(null); 
-
-    useEffect(() => {
-        const url = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "http://localhost:5000";
-        socketRef.current = io.connect(url);
- 
-        socketRef.current.emit("join_room", "room_name");
-
-        socketRef.current.on("receive_message", (data) => {
-            setMessageReceived(data.message);
-        });
-
-        return () => {
-            socketRef.current.disconnect();
-        };
-    }, []); 
-
-    const sendMessage = () => {
-        if (socketRef.current) {
-            socketRef.current.emit("send_message", { message, room: "room_name" });
-        }
-    };
-
     return (
         <div className="App">
-            <input 
-                placeholder="Message..." 
-                onChange={(e) => { setMessage(e.target.value); }} 
-            />
-            <button onClick={sendMessage}> Send Message </button>
-            <h1>Message: </h1>
-            {messageReceived}
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home/>}
+                />
+            </Routes>
         </div>
     );
 }
---
+++ frontend/src/views/home.js
@@ -0,0 +1,37 @@
+import { io } from 'socket.io-client';
+import { useEffect, useState, useRef } from "react";
+
+const Home = () => {
+    const [message, setMessage] = useState('');
+    const [messageReceived, setMessageReceived] = useState('');
+    const socketRef = useRef(null);
+
+    useEffect(() => {
+        const url = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : "http://localhost:5000";
+        socketRef.current = io.connect(url);
+
+        socketRef.current.emit("join_room", "room_name");
+
+        socketRef.current.on("receive_message", (data) => {
+            setMessageReceived(data.message);
+        });
+
+        return () => {
+            socketRef.current.disconnect();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        if (socketRef.current) {
+            socketRef.current.emit("send_message", { message, room: "room_name" });
+        }
+    };
+
+    return (
+        <div className="App">
+            <button> Create Room </button>
+        </div>
+    );
+}
+
+export default Home;
