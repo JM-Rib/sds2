bc493949d0a6dad27f0613f23189f4c04e01290e
SantamarÍa SantamarÍa <paula.santamaria@alumnos.upm.es>
2024-12-18 19:34:03 +0100

    modified voting to have a more "card looking" appareance when chosing a number

--- frontend/src/components/VoteSelect.js
+++ frontend/src/components/VoteSelect.js
@@ -1,24 +1,23 @@
-import {Button} from "primereact/button";
+import { Button } from "primereact/button";
+import './VoteSelect.css'; // Ensure the correct path to the CSS file
 
 const VoteSelect = ({ setVote }) => {
     const voteValues = [1, 2, 3, 5, 8, 13, 21];
 
     return (
-        <>
-            <div className="grid grid-cols-7 gap-9">
-                {voteValues.map((voteValue, i) => (
-                    <Button
-                        key={voteValue} // Use voteValue as the unique key
-                        label={voteValue.toString()}
-                        onClick={() => setVote(voteValue)}
-                        raised
-                        rounded
-                    />
-                ))}
-            </div>
-        </>
+        <div className="vote-select-container">
+            {voteValues.map((voteValue) => (
+                <Button
+                    key={voteValue}
+                    label={voteValue.toString()}
+                    onClick={() => setVote(voteValue)}
+                    className="vote-select-button" // Add the class here
+                    raised
+                    rounded
+                />
+            ))}
+        </div>
     );
 }
 
 export default VoteSelect;
-
